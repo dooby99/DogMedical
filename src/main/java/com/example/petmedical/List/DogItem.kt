77dbd7819f8b7eafd115079815907a314da05f11
dooby99 <102219869+dooby99@users.noreply.github.com>
2023-06-02 19:43:43 +0900

    Add files via upload

---
+++ src/main/java/com/example/petmedical/List/DogItem.kt
@@ -0,0 +1,6 @@
+package com.example.petmedical.List
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+class DogItem(val image: Bitmap, val id: String, val dateTime: String)
---
+++ src/main/java/com/example/petmedical/List/ListView.kt
@@ -0,0 +1,12 @@
+package com.example.petmedical.List
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petmedical.R
+
+class ListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
